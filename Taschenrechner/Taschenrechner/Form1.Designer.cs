908546fab4b68fb765736a3efa8b0f4402056cc2
Philipp Schiffbauer <freackyphil@gmail.com>
2017-03-08 14:19:00 +0100

    Hab keinen Bock mehr

--- Taschenrechner/Taschenrechner/Form1.Designer.cs
+++ Taschenrechner/Taschenrechner/Form1.Designer.cs
@@ -28,13 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.siebenButton = new System.Windows.Forms.Button();
+            this.achtButton = new System.Windows.Forms.Button();
+            this.neunButton = new System.Windows.Forms.Button();
+            this.vierButton = new System.Windows.Forms.Button();
+            this.fünfButton = new System.Windows.Forms.Button();
+            this.sechsButton = new System.Windows.Forms.Button();
+            this.einsButton = new System.Windows.Forms.Button();
+            this.zweiButton = new System.Windows.Forms.Button();
+            this.dreiButton = new System.Windows.Forms.Button();
+            this.nullButton = new System.Windows.Forms.Button();
+            this.divisionButton = new System.Windows.Forms.Button();
+            this.multiplikationButton = new System.Windows.Forms.Button();
+            this.subtraktionButton = new System.Windows.Forms.Button();
+            this.additionButton = new System.Windows.Forms.Button();
+            this.kommaButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // siebenButton
+            // 
+            this.siebenButton.Location = new System.Drawing.Point(12, 169);
+            this.siebenButton.Name = "siebenButton";
+            this.siebenButton.Size = new System.Drawing.Size(60, 60);
+            this.siebenButton.TabIndex = 0;
+            this.siebenButton.Text = "7";
+            this.siebenButton.UseVisualStyleBackColor = true;
+            // 
+            // achtButton
+            // 
+            this.achtButton.Location = new System.Drawing.Point(78, 169);
+            this.achtButton.Name = "achtButton";
+            this.achtButton.Size = new System.Drawing.Size(60, 60);
+            this.achtButton.TabIndex = 1;
+            this.achtButton.Text = "8";
+            this.achtButton.UseVisualStyleBackColor = true;
+            // 
+            // neunButton
+            // 
+            this.neunButton.Location = new System.Drawing.Point(144, 169);
+            this.neunButton.Name = "neunButton";
+            this.neunButton.Size = new System.Drawing.Size(60, 60);
+            this.neunButton.TabIndex = 2;
+            this.neunButton.Text = "9";
+            this.neunButton.UseVisualStyleBackColor = true;
+            // 
+            // vierButton
+            // 
+            this.vierButton.Location = new System.Drawing.Point(12, 235);
+            this.vierButton.Name = "vierButton";
+            this.vierButton.Size = new System.Drawing.Size(60, 60);
+            this.vierButton.TabIndex = 3;
+            this.vierButton.Text = "4";
+            this.vierButton.UseVisualStyleBackColor = true;
+            // 
+            // fünfButton
+            // 
+            this.fünfButton.Location = new System.Drawing.Point(78, 235);
+            this.fünfButton.Name = "fünfButton";
+            this.fünfButton.Size = new System.Drawing.Size(60, 60);
+            this.fünfButton.TabIndex = 4;
+            this.fünfButton.Text = "5";
+            this.fünfButton.UseVisualStyleBackColor = true;
+            // 
+            // sechsButton
+            // 
+            this.sechsButton.Location = new System.Drawing.Point(144, 235);
+            this.sechsButton.Name = "sechsButton";
+            this.sechsButton.Size = new System.Drawing.Size(60, 60);
+            this.sechsButton.TabIndex = 5;
+            this.sechsButton.Text = "6";
+            this.sechsButton.UseVisualStyleBackColor = true;
+            // 
+            // einsButton
+            // 
+            this.einsButton.Location = new System.Drawing.Point(12, 301);
+            this.einsButton.Name = "einsButton";
+            this.einsButton.Size = new System.Drawing.Size(60, 60);
+            this.einsButton.TabIndex = 6;
+            this.einsButton.Text = "1";
+            this.einsButton.UseVisualStyleBackColor = true;
+            // 
+            // zweiButton
+            // 
+            this.zweiButton.Location = new System.Drawing.Point(78, 301);
+            this.zweiButton.Name = "zweiButton";
+            this.zweiButton.Size = new System.Drawing.Size(60, 60);
+            this.zweiButton.TabIndex = 7;
+            this.zweiButton.Text = "2";
+            this.zweiButton.UseVisualStyleBackColor = true;
+            // 
+            // dreiButton
+            // 
+            this.dreiButton.Location = new System.Drawing.Point(144, 301);
+            this.dreiButton.Name = "dreiButton";
+            this.dreiButton.Size = new System.Drawing.Size(60, 60);
+            this.dreiButton.TabIndex = 8;
+            this.dreiButton.Text = "3";
+            this.dreiButton.UseVisualStyleBackColor = true;
+            // 
+            // nullButton
+            // 
+            this.nullButton.Location = new System.Drawing.Point(78, 367);
+            this.nullButton.Name = "nullButton";
+            this.nullButton.Size = new System.Drawing.Size(60, 60);
+            this.nullButton.TabIndex = 9;
+            this.nullButton.Text = "0";
+            this.nullButton.UseVisualStyleBackColor = true;
+            // 
+            // divisionButton
+            // 
+            this.divisionButton.Location = new System.Drawing.Point(234, 169);
+            this.divisionButton.Name = "divisionButton";
+            this.divisionButton.Size = new System.Drawing.Size(93, 60);
+            this.divisionButton.TabIndex = 10;
+            this.divisionButton.Text = "/";
+            this.divisionButton.UseVisualStyleBackColor = true;
+            // 
+            // multiplikationButton
+            // 
+            this.multiplikationButton.Location = new System.Drawing.Point(234, 235);
+            this.multiplikationButton.Name = "multiplikationButton";
+            this.multiplikationButton.Size = new System.Drawing.Size(93, 60);
+            this.multiplikationButton.TabIndex = 11;
+            this.multiplikationButton.Text = "x";
+            this.multiplikationButton.UseVisualStyleBackColor = true;
+            // 
+            // subtraktionButton
+            // 
+            this.subtraktionButton.Location = new System.Drawing.Point(234, 301);
+            this.subtraktionButton.Name = "subtraktionButton";
+            this.subtraktionButton.Size = new System.Drawing.Size(93, 60);
+            this.subtraktionButton.TabIndex = 12;
+            this.subtraktionButton.Text = "-";
+            this.subtraktionButton.UseVisualStyleBackColor = true;
+            // 
+            // additionButton
+            // 
+            this.additionButton.Location = new System.Drawing.Point(234, 367);
+            this.additionButton.Name = "additionButton";
+            this.additionButton.Size = new System.Drawing.Size(93, 60);
+            this.additionButton.TabIndex = 13;
+            this.additionButton.Text = "+";
+            this.additionButton.UseVisualStyleBackColor = true;
+            // 
+            // kommaButton
+            // 
+            this.kommaButton.Location = new System.Drawing.Point(144, 367);
+            this.kommaButton.Name = "kommaButton";
+            this.kommaButton.Size = new System.Drawing.Size(60, 60);
+            this.kommaButton.TabIndex = 14;
+            this.kommaButton.Text = ",";
+            this.kommaButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 454);
+            this.ClientSize = new System.Drawing.Size(446, 529);
+            this.Controls.Add(this.kommaButton);
+            this.Controls.Add(this.additionButton);
+            this.Controls.Add(this.subtraktionButton);
+            this.Controls.Add(this.multiplikationButton);
+            this.Controls.Add(this.divisionButton);
+            this.Controls.Add(this.nullButton);
+            this.Controls.Add(this.dreiButton);
+            this.Controls.Add(this.zweiButton);
+            this.Controls.Add(this.einsButton);
+            this.Controls.Add(this.sechsButton);
+            this.Controls.Add(this.fünfButton);
+            this.Controls.Add(this.vierButton);
+            this.Controls.Add(this.neunButton);
+            this.Controls.Add(this.achtButton);
+            this.Controls.Add(this.siebenButton);
             this.Name = "Form1";
             this.Text = "Taschenrechner";
             this.ResumeLayout(false);
@@ -42,6 +207,22 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button siebenButton;
+        private System.Windows.Forms.Button achtButton;
+        private System.Windows.Forms.Button neunButton;
+        private System.Windows.Forms.Button vierButton;
+        private System.Windows.Forms.Button fünfButton;
+        private System.Windows.Forms.Button sechsButton;
+        private System.Windows.Forms.Button einsButton;
+        private System.Windows.Forms.Button zweiButton;
+        private System.Windows.Forms.Button dreiButton;
+        private System.Windows.Forms.Button nullButton;
+        private System.Windows.Forms.Button divisionButton;
+        private System.Windows.Forms.Button multiplikationButton;
+        private System.Windows.Forms.Button subtraktionButton;
+        private System.Windows.Forms.Button additionButton;
+        private System.Windows.Forms.Button kommaButton;
     }
 }
 
